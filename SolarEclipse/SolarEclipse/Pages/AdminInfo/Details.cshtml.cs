3909dde5facc5c622cd8dcfc47c3fb06309dbd11
Trey <thoopchuk@gmail.com>
2021-11-09 15:53:46 -0600

    Created Admin Info Page
    Currently only accessible by knowing the url path

---
+++ SolarEclipse/SolarEclipse/Pages/AdminInfo/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SolarEclipse.Data;
+using SolarEclipse.Models;
+
+namespace SolarEclipse.Pages.AdminInfo
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SolarEclipse.Data.SolarEclipseContext _context;
+
+        public DetailsModel(SolarEclipse.Data.SolarEclipseContext context)
+        {
+            _context = context;
+        }
+
+        public Contact Contact { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Contact = await _context.Contacts.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Contact == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ SolarEclipse/SolarEclipse/Pages/AdminInfo/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SolarEclipse.Data;
+using SolarEclipse.Models;
+
+namespace SolarEclipse.Pages.AdminInfo
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SolarEclipse.Data.SolarEclipseContext _context;
+
+        public IndexModel(SolarEclipse.Data.SolarEclipseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; }
+        public IList<MusicSub> MusicSub { get; set; }
+        public IList<Volunteer> Volunteer { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Contact = await _context.Contacts.ToListAsync();
+            MusicSub = await _context.MusicSubs.ToListAsync();
+            Volunteer = await _context.Volunteers.ToListAsync();
+        }
+    }
+}
